11a0e2240d411724ce1bb5dad05a0be87c72393d
Joel <joel.v.paul@outlook.com>
2022-09-20 11:13:09 +1000

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    elements.forEach(element => {
+        if (array.includes(element)) {
+            array.splice(array.indexOf(element), 1);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
