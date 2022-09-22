e7ccab03910eccaf9714b620443d8339d6c32030
Joel <joel.v.paul@outlook.com>
2022-09-22 18:51:07 +1000

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce((oldest, current) => {
+    const lastYearO = 'yearOfDeath' in oldest ? oldest.yearOfDeath : currentYear;
+    const ageO = lastYearO - oldest.yearOfBirth;
+
+    const lastYearC = 'yearOfDeath' in current ? current.yearOfDeath : currentYear;
+    const ageC = lastYearC - current.yearOfBirth;
+
+    return ageC > ageO ? current : oldest;
+  });
 };
 
 // Do not edit below this line
