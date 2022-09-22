48e2a8a795a96dc7fcf46b59836ae38d5042fb9f
Joel <joel.v.paul@outlook.com>
2022-09-22 16:38:42 +1000

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[!., ]/g, '');
 
+  const halfLen = Math.floor(string.length / 2);
+
+  for(let i = 0; i <= halfLen; i++) {
+    if (string[i] !== string[string.length - i - 1]) return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
